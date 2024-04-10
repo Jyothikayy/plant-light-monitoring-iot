0c2c0fc387c4d5008070eae8e3462612fcce47a0
jyothikay <125146575+Jyothikayy@users.noreply.github.com>
2024-04-10 15:10:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+setChartLibrary("google-chart");
+setChartType("barGraph");
+setChartTitle("Light Internsity Chart");
+setAxisName("TIME","INTENSITY");
+setCrosshair(true);
+setAnimation(true);
+plotChart("time_stamp","light");
